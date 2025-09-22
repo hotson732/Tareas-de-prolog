261d3fdb6773e93dd492ea37fed8176cdbdc4239
Edson <hotsonedson@gmail.com>
2025-09-22 10:06:19 -0600

    tarea

---
+++ arbol_genealogico.lisp
@@ -0,0 +1,65 @@
+(defparameter *nodes* '(
+    (Edson
+     (apellids (Campos Hernandez))
+     (altura (Mediano)) 
+     (complexion (Atletico)) 
+     (ojos (Oscuros)) 
+     (cabello (Negro))
+     (color_piel (moreno))
+     )
+    
+    (Miguel
+     (apellidos (Campos Acosta))
+     (altura (Mediano")) 
+     (complexion (Mediano)) 
+     (ojos (Marrones) )
+     (cabello (Negro))
+     (color_piel (moreno))
+     )
+    
+    (Rosa
+     (apellidos (Hernandez Melchor))
+     (altura (baja)) 
+     (complexion (llenita)) 
+     (ojos (Marrones)) 
+     (cabello (negro))
+     (color_piel (blanco))
+     )
+    
+    (Jose
+     (apellidos (Campos Hernandez)) 
+     (altura (alto)) 
+     (complexion (delgado)) 
+     (ojos (Oscuros)) 
+     (cabello (negro))
+     (color_piel (moreno))
+     )
+    
+    (Ivan
+     (apellidos (Campos Hernandez))
+     (altura (mediano)) 
+     (complexion (atletico))
+     (ojos (Marrones)) 
+     (cabello (castaño_oscuro))
+     (color_piel (blanco))
+     )
+    
+    (katya
+     (apellidos (Campos Hernandez))
+     (altura (Media)) 
+     (complexion (delgada)) 
+     (ojos (Marrones)) 
+     (cabello (castaño))
+     (color_piel (blanco))
+     )
+
+    (Rosa
+     (apellidos (Campos Hernandez))
+     (altura (baja)) 
+     (complexion (llenita)) 
+     (ojos (Marrones)) 
+     (cabello (negro))
+     (color_piel (blanco))
+    )
+    
+))
